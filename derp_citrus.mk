2d908702e8db9966e547fe444439db1b74996c2e
hac4us06 <hac4usanime@gmail.com>
2023-07-01 19:22:21 +0200

    citrus: update dt
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- derp_citrus.mk
+++ derp_citrus.mk
@@ -8,16 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common derp stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from citrus device
 $(call inherit-product, device/xiaomi/citrus/device.mk)
 
-PRODUCT_NAME := lineage_citrus
+PRODUCT_NAME := derp_citrus
 PRODUCT_DEVICE := citrus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2010J19CG
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+
+TARGET_BOOT_ANIMATION_RES := 1080
+DERP_BUILDTYPE := Official
+USE_LEGACY_BOOTANIMATION := true
+
